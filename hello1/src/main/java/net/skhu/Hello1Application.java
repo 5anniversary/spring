51b02ec2f95c7f23bee464acabcf2f96a47b3f2e
junhyeon <oh.junhyeon95@gmail.com>
2020-09-27 13:08:45 +0900

    1차 과제

---
+++ hello1/src/main/java/net/skhu/Hello1Application.java
@@ -0,0 +1,16 @@
+package net.skhu;
+
+import java.util.Date;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.ui.Model;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hello1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hello1Application.class, args);
+	}
+
+}
